429aaac8fc21b043af78e6859a876a1ce1ac3b6d
akrvkn <akrvkn@yandex.ru>
2020-12-02 17:53:31 +0300

    restapi

--- api/db/api-import.php
+++ api/db/api-import.php
@@ -14,6 +14,8 @@ $tours_base = $API_BASE.'Tours/';
 $schemes = $API_BASE.'ShipsSchemes/';
 $excursions = $API_BASE.'Excursions/';
 //https://api.infoflot.com/JSON/f438cedcb449037583a8f84d5f5a3a3ff34139ab/Tours/5
+//https://restapi.infoflot.com/ships?key=f438cedcb449037583a8f84d5f5a3a3ff34139ab
+
 $pattern = '~(*UTF8)[\p{Cyrillic}]+~i';
 
 $dir = 'data';
