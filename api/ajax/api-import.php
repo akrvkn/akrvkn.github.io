37c93b24210ad13111e55a4c8b8f6a57563b7dc0
akrvkn <akrvkn@yandex.ru>
2021-01-20 09:34:22 +0300

    actions

--- api/ajax/api-import.php
+++ api/ajax/api-import.php
@@ -17,7 +17,7 @@ $schemes = $API_BASE.'ShipsSchemes/';
 $excursions = $API_BASE.'Excursions/';
 $now = date('Y-m-d');
 $cruises_base = 'https://restapi.infoflot.com/cruises?key=407c8c353a23a14d40479eb4e4290a8a6d32b06b&dateStartFrom='.$now.'&ship=';
-$ship_single = 'https://restapi.infoflot.com/ships/';
+$ship_single = 'https://restapi.infoflot.com/ships?key=407c8c353a23a14d40479eb4e4290a8a6d32b06b';
 $cruise_single = 'https://restapi.infoflot.com/cruises/';
 
 //Vodohod.com API v2
